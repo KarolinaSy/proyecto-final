28f39dace8865a1762b8a11bbb5ac056dc8cf951
KarolinaSy <167114375+KarolinaSy@users.noreply.github.com>
2024-10-13 22:00:30 +0200

    responsive y mejoras de dropdown

--- js/script.js
+++ js/script.js
@@ -197,3 +197,14 @@ function actualizarTotalCarrito(){
     document.getElementsByClassName('carrito-precio-total')[0].innerText = total.toLocaleString("es") + ",00" + '€';
 
 }
+
+
+// Despliegue menú categorías //
+const dropdownButton = document.querySelector('.dropdown-button');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdownButton.addEventListener('click',   
+ () => {
+  dropdownMenu.classList.toggle('show');   
+
+});
